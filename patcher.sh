106325ac5b2681254a3b5f60f73b03bb4dbdbffe
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-04 11:49:12 +0100

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 # Supported Firmwares:
 # AB OTA
@@ -9,72 +9,71 @@ usage() {
     echo -e "\t[--verbose|-v]: Verbose mode"
 }
 
-LOCALDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+LOCALDIR=$(dirname "$0")
 TOOLSDIR="$LOCALDIR/tools"
-if [[ ! -d "$TOOLSDIR/update_payload_extractor" ]]; then
+if [ ! -d "$TOOLSDIR/update_payload_extractor" ]; then
     git clone -q https://github.com/erfanoabdi/update_payload_extractor.git "$TOOLSDIR/update_payload_extractor"
 else
-    git -C "$TOOLSDIR/update_payload_extractor" pull
+    cd "$TOOLSDIR/update_payload_extractor" && git pull
 fi
 PAYLOAD_EXTRACTOR="$TOOLSDIR/update_payload_extractor/extract.py"
 VERBOSE=n
 OUTDIR="$LOCALDIR/out"
 
 POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
+while [ $# -gt 0 ]; do
+    key="$1"
 
-case $key in
-    --verbose|-v)
-    VERBOSE=y
-    shift
-    ;;
-    --output|-o)
-    OUTDIR="$(realpath $2)"
-    shift
-    shift
-    ;;
-    --help|-h|-?)
-    usage
-    exit
-    ;;
-    *)
-    POSITIONAL+=("$1")
-    shift
-    ;;
-esac
+    case $key in
+        --verbose|-v)
+        VERBOSE=y
+        shift
+        ;;
+        --output|-o)
+        OUTDIR="$(realpath "$2")"
+        shift
+        shift
+        ;;
+        --help|-h|-?)
+        usage
+        exit
+        ;;
+        *)
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-if [[ ! -n $2 ]]; then
+if [ ! -n "$2" ]; then
     echo "ERROR: Enter all needed parameters"
     usage
     exit
 fi
 
-BASE_FIRMWARE="$(realpath $1)"
+BASE_FIRMWARE="$(realpath "$1")"
 shift
 
 TMPDIR="$OUTDIR/tmp"
 mkdir -p "$TMPDIR"
 
 echo "Extracting Base Firmware"
-if [[ "$VERBOSE" = "n" ]]; then
-    "$LOCALDIR"/extractor.sh "$BASE_FIRMWARE" "$TMPDIR/source" > /dev/null 2>&1
+if [ "$VERBOSE" = "n" ]; then
+    sh "$LOCALDIR/extractor.sh" "$BASE_FIRMWARE" "$TMPDIR/source" > /dev/null 2>&1
 else
-    "$LOCALDIR"/extractor.sh "$BASE_FIRMWARE" "$TMPDIR/source"
+    sh "$LOCALDIR/extractor.sh" "$BASE_FIRMWARE" "$TMPDIR/source"
 fi
 
-for OTA_FILE in $@; do
-    ((OTA_NO++))
+for OTA_FILE in "$@"; do
+    OTA_NO=$((OTA_NO+1))
     echo "Patching OTA Number $OTA_NO"
-    if [[ ! $(7z l -ba $OTA_FILE | grep "payload.bin") ]]; then
+    if ! (7z l -ba "$OTA_FILE" | grep -q "payload.bin"); then
         echo "$OTA_FILE is bad OTA zip"
         continue
     fi
     7z e -y "$OTA_FILE" "payload.bin" -o"$TMPDIR/" > /dev/null 2>&1
-    if [[ "$VERBOSE" = "n" ]]; then
+    if [ "$VERBOSE" = "n" ]; then
         python "$PAYLOAD_EXTRACTOR" --source_dir "$TMPDIR/source" --output_dir "$TMPDIR/output" "$TMPDIR/payload.bin" > /dev/null 2>&1
     else
         python "$PAYLOAD_EXTRACTOR" --source_dir "$TMPDIR/source" --output_dir "$TMPDIR/output" "$TMPDIR/payload.bin"
